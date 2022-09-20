276d2b25f28ff7d206ab257911259d064a222e04
wuyun <wuyun7528189@163.com>
2022-09-20 17:07:03 +0800

    白嫖

--- src/main/java/com/wyhwy/diagnosis_backend/mapper/UserMapper.java
+++ src/main/java/com/wyhwy/diagnosis_backend/mapper/UserMapper.java
@@ -1,6 +1,12 @@
 package com.wyhwy.diagnosis_backend.mapper;
 
+import com.wyhwy.diagnosis_backend.domain.User;
+import org.apache.ibatis.annotations.Param;
+
 public interface UserMapper {
 //    User selectById(int id);
 //    void insert(User user);
+    User findByUsername(@Param("username") String username);
+    User findUserById(@Param("Id") Integer Id);
+
 }
--- src/main/java/com/wyhwy/diagnosis_backend/service/UserService.java
+++ src/main/java/com/wyhwy/diagnosis_backend/service/UserService.java
@@ -1,2 +1,20 @@
-package com.wyhwy.diagnosis_backend.service;public class UserService {
+package com.wyhwy.diagnosis_backend.service;
+
+import com.wyhwy.diagnosis_backend.domain.User;
+import com.wyhwy.diagnosis_backend.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserMapper userMapper;
+    public User findByUsername(User user){
+        return userMapper.findByUsername(user.getUsername());
+    }
+    public User findUserById(Integer userId){
+        return userMapper.findUserById(userId);
+    }
+
 }
